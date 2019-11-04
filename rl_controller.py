b6c0d004c7eed7222a6d3d9494886d525df758af
Kirill <kirill5pol>
2019-11-04 10:33:52 -0700

    Added option for sim/hardware for the test on rl_controller

--- rl_controller.py
+++ rl_controller.py
@@ -1,4 +1,5 @@
 import numpy as np
+import argparse
 from rl_weights import w0, b0, w1, b1, wout, bout
 
 
@@ -9,10 +10,10 @@ def rl_controller(x):
     return h1 @ wout + bout
 
 
-def test_rl_controller():
+def test_rl_controller(use_simulator):
     from gym_brt.envs import QubeSwingupEnv
 
-    with QubeSwingupEnv(use_simulator=True, frequency=250) as env:
+    with QubeSwingupEnv(use_simulator=use_simulator, frequency=250) as env:
         while True:
             state = env.reset()
             state, _, done, _ = env.step(np.array([0]))
@@ -20,8 +21,13 @@ def test_rl_controller():
             while not done:
                 action = rl_controller(state)
                 state, _, done, _ = env.step(action)
-                env.render()
+                if use_simulator:
+                    env.render()
 
 
 if __name__ == "__main__":
-    test_rl_controller()
+    # Parse command line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--use_simulator", action="store_true")
+    args, _ = parser.parse_known_args()
+    test_rl_controller(args.use_simulator)
